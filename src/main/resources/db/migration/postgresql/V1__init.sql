865467233c86054901ab6fff42698f1a767b3a01
Jorismar Barbosa <jorismar.barbosa@gmail.com>
2019-10-12 20:13:44 -0300

    Add SQL script to init the db tables

---
+++ src/main/resources/db/migration/postgresql/V1__init.sql
@@ -0,0 +1,24 @@
+CREATE TABLE portador (
+  cpf varchar(255) PRIMARY KEY NOT NULL,
+  nome varchar(255) NOT NULL,
+  email varchar(255) NOT NULL,
+  data_nascimento DATE NOT NULL
+);
+
+CREATE TABLE cartao (
+  numero varchar(255) PRIMARY KEY NOT NULL,
+  nome_portador varchar(255) NOT NULL,
+  validade varchar(255) NOT NULL,
+  cvc int NOT NULL,
+  cpf varchar(255) NOT NULL,
+  FOREIGN KEY(cpf) REFERENCES portador(cpf)
+);
+
+CREATE TABLE lancamento (
+  codigo bigint PRIMARY KEY NOT NULL,
+  numero_cartao varchar(255) NOT NULL,
+  data_lancamento date NOT NULL,
+  estabelecimento varchar(255) NOT NULL,
+  valor decimal NOT NULL,
+  FOREIGN KEY(numero_cartao) REFERENCES cartao(numero)
+);
