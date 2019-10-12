6bf0b772deaa7fb8918cfae648784a7f377a4570
Jorismar Barbosa <jorismar.barbosa@gmail.com>
2019-10-12 11:29:59 -0300

    Creates the Spring Project

---
+++ src/test/java/com/jorismar/cdtapideveval/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jorismar.cdtapideveval;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
